c588085b51c64f77a2f30200137fc4e1098111c7
jtaubensee <jotaub@microsoft.com>
2016-12-14 15:17:05 -0800

    Adding copyrights

--- src/service-bus-dotnet-management/Program.cs
+++ src/service-bus-dotnet-management/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace service_bus_dotnet_management
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace service_bus_dotnet_management
 {
 	public class Program
 	{
